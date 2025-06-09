d74ef6388acaddd9aef9dcaca78f3883e871ce5f
Alliraj <alliraj575@gmail.com>
2025-06-09 16:12:35 +0100

    Fixed syntax in urls.py

--- Djangoproject/urls.py
+++ Djangoproject/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('TTT.urls')),
-    
+]
+
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-]
